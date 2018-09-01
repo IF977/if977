105c8e0e3ee10d622cd5b0c244e13e4ea2cae124
Ricardo Ebbers <recl@cin.ufpe.br>
2018-09-01 01:09:55 -0300

    Fixes ArgumentErrors and changes all :should validations to :expect

--- hws/vcg_hw0/spec/vcg_hw0_part2_spec.rb
+++ hws/vcg_hw0/spec/vcg_hw0_part2_spec.rb
@@ -7,10 +7,10 @@ describe "#hello" do
   end
 
   it "The hello method returns the correct string [30 points]" do
-    hello("Dan").class.should == String
-    hello("Dan").should eq('Hello, Dan'), "Incorrect results for input: \"Dan\""
-    hello("BILL").should eq('Hello, BILL'), "Incorrect results for input: \"BILL\""
-    hello("Mr. Ilson").should eq('Hello, Mr. Ilson'), "Incorrect results for input: \"Mr. Ilson\""
+    expect(hello("Dan").class).to be == String
+    expect(hello("Dan")).to eq('Hello, Dan'), "Incorrect results for input: \"Dan\""
+    expect(hello("BILL")).to eq('Hello, BILL'), "Incorrect results for input: \"BILL\""
+    expect(hello("Mr. Ilson")).to eq('Hello, Mr. Ilson'), "Incorrect results for input: \"Mr. Ilson\""
   end
 end
 
@@ -20,22 +20,22 @@ describe "#starts_with_consonant?" do
     expect { starts_with_consonant?("d") }.not_to raise_error()
   end
   it 'classifies true cases [10 points]' do
-    starts_with_consonant?('v').should be true, "'v' is a consonant"
+    expect(starts_with_consonant?('v')).to be true #, "'v' is a consonant"
     ['v', 'vest', 'Veeee', 'crypt'].each do |string|
-      starts_with_consonant?(string).should be true, "Incorrect results for input: \"#{string}\""
+      expect(starts_with_consonant?(string)).to be true #, "Incorrect results for input: \"#{string}\""
     end
   end
   it 'classifies false cases [10 points]' do
-    starts_with_consonant?('a').should be false, "'a' is not a consonant"
+    expect(starts_with_consonant?('a')).to be false #, "'a' is not a consonant"
     ['asdfgh', 'Unix'].each do |string|
-        starts_with_consonant?(string).should be false, "Incorrect results for input: \"#{string}\""
+        expect(starts_with_consonant?(string)).to be false #, "Incorrect results for input: \"#{string}\""
     end
   end
   it 'works on the empty string [5 points]' do
-    starts_with_consonant?('').should be false
+    expect(starts_with_consonant?('')).to be false
   end
   it 'works on nonletters [5 points]' do
-    starts_with_consonant?('#foo').should be false
+    expect(starts_with_consonant?('#foo')).to be false
   end
 end
 
@@ -46,14 +46,14 @@ describe "#binary_multiple_of_4?" do
   end
   it "classifies valid binary numbers [30 points]" do
     ["1010101010100", "0101010101010100", "100", "0"].each do |string|
-      binary_multiple_of_4?(string).should be true,  "Incorrect results for input: \"#{string}\""
+      expect(binary_multiple_of_4?(string)).to be true #, "Incorrect results for input: \"#{string}\""
     end
     ["101", "1000000000001"].each do |string|
-      binary_multiple_of_4?(string).should_not be true,  "Incorrect results for input: \"#{string}\""
+      expect(binary_multiple_of_4?(string)).not_to be true #, "Incorrect results for input: \"#{string}\""
     end
   end
   it "rejects invalid binary numbers [10 points]" do
-    binary_multiple_of_4?('a100').should be false, "'a100' is not a valid binary number!"
-    binary_multiple_of_4?('').should be false, "The empty string is not a valid binary number!"
+    expect(binary_multiple_of_4?('a100')).to be false #, "'a100' is not a valid binary number!"
+    expect(binary_multiple_of_4?('')).to be false #, "The empty string is not a valid binary number!"
   end
 end
--- hws/vcg_hw0/spec/vcg_hw0_part3_spec.rb
+++ hws/vcg_hw0/spec/vcg_hw0_part3_spec.rb
@@ -8,18 +8,18 @@ describe "BookInStock" do
   describe 'getters and setters' do
     before(:each)  { @book = BookInStock.new('isbn1', 33.8) }
     it 'should set ISBN [10 points]' do
-      @book.isbn.should == 'isbn1'
+      expect(@book.isbn).to be == 'isbn1'
     end
     it 'should set price [10 points]' do
-      @book.price.should == 33.8
+      expect(@book.price).to be == 33.8
     end
     it 'should be able to change ISBN [10 points]' do
       @book.isbn = 'isbn2'
-      @book.isbn.should == 'isbn2'
+      expect(@book.isbn).to be == 'isbn2'
     end
     it 'should be able to change price [10 points]' do
       @book.price = 300.0
-      @book.price.should == 300.0
+      expect(@book.price).to be == 300.0
     end
   end
   describe 'constructor' do
@@ -35,16 +35,16 @@ describe "BookInStock" do
   end
   describe "#price_as_string" do
     it "should be defined" do
-      BookInStock.new('isbn1', 10).should respond_to(:price_as_string)
+      expect(BookInStock.new('isbn1', 10)).to respond_to(:price_as_string)
     end
     it 'should display 33.95 as "$33.95" [10 points]' do
-      BookInStock.new('isbn11', 33.95).price_as_string.should == '$33.95'
+      expect(BookInStock.new('isbn11', 33.95).price_as_string).to be == '$33.95'
     end
     it "should display 1.1 as $1.10 [10 points]" do
-      BookInStock.new('isbn11', 1.1).price_as_string.should == '$1.10'
+      expect(BookInStock.new('isbn11', 1.1).price_as_string).to be == '$1.10'
     end
     it "should display 20 as $20.00 [10 points]" do
-      BookInStock.new('isbn11', 20).price_as_string.should == '$20.00'
+      expect(BookInStock.new('isbn11', 20).price_as_string).to be == '$20.00'
     end
   end
 end
